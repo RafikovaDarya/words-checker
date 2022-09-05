9eb58f23503713b953203534a6c3f6c0e9be8cd9
Rafikova Darya <darya.sladkova.89@mail.ru>
2022-09-05 17:58:46 +0400

    first commit

---
+++ src/WordsChecker.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class WordsChecker {
+    private Set<String> text;
+
+    public WordsChecker(String text) {
+        this.text = new HashSet<>(List.of(text.split("\\P{IsAlphabetic}+"))) ;
+    }
+    public boolean hasWord(String word) {
+        return text.contains(word);
+    }
+}
